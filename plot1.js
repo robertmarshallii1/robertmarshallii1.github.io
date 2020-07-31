9503ecc47431dc2ea36fbc28ae3724aea4737a2c
Robert F. Marshall II <robertmarshallii1@gmail.com>
2020-07-30 17:25:14 -0700

    added data file

--- plot1.js
+++ plot1.js
@@ -236,6 +236,18 @@ async function dvsyr() {
         .attr("text-anchor", "start")  
         .style('font','15px sans-serif')
         .data(data)
+        .text('Total deaths: 0')
+        .transition()
+            .tween("text", function() {
+                var selection = d3.select(this);    // selection of node being transitioned
+                var start = 0; // start value prior to transition
+                var end = d3.sum(data, function(d) {return d.ALL;});                     // specified end value
+                var interpolator = d3.interpolateNumber(start,end); // d3 interpolator
+        
+                return function(t) { selection.text('Total deaths: ' + Math.round(interpolator(t))); };  // return value
+                
+            })
+            .duration(tt)
         .text('Total deaths: ' + d3.sum(data, function(d) {return d.ALL;}));
 
 }
